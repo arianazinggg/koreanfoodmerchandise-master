aa3ead315cbfbd8d4b1a6096dd7a1dbc8a3acbd5
Ariane Mendez <ariane.mendez80@gmail.com>
2022-10-28 22:30:58 +0800

    made changes in server.js config

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'))
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port,);
 app.set('view engine', 'ejs');
